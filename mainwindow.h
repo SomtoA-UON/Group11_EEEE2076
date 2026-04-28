724498478cef13a16499c55192bb3485324aeaaa
Ivanhuang2 <eeyih1@nottingham.ac.uk>
2026-04-28 15:49:55 +0100

    Refactor MainWindow class and add VR support

--- mainwindow.h
+++ mainwindow.h
@@ -2,41 +2,57 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QModelIndex>
+
 #include "ModelPart.h"
 #include "ModelPartList.h"
+#include "VRRenderThread.h"
 
 #include <vtkSmartPointer.h>
 #include <vtkGenericOpenGLRenderWindow.h>
 #include <vtkRenderer.h>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
+namespace Ui
+{
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
 public slots:
-    void handleButton1();
     void handleButton2();
     void handleTreeClicked();
+
     void on_actionOpen_File_triggered();
     void on_actionItem_Options_triggered();
 
+    void startVR();
+
 signals:
     void statusUpdateMessage(const QString& message, int timeout);
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
+
     ModelPartList* partList;
+
     vtkSmartPointer<vtkRenderer> renderer;
-    vtkSmartPointer<vtkGenericOpenGLRenderWindow>renderWindow;
+    vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindow;
+
+    VRRenderThread* vrThread = nullptr;
+
     void updateRender();
     void updateRenderFromTree(const QModelIndex& index);
+
+    int addPartToVRThread(const QModelIndex& index);
 };
+
 #endif // MAINWINDOW_H
