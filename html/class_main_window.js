3b1a4e1a56add945b71411e355510c7bf657f6a4
Lucas Fisher <eeylf1@nottingham.ac.uk>
2026-05-05 14:43:55 +0100

    Final Version

--- html/class_main_window.js
+++ html/class_main_window.js
@@ -4,8 +4,6 @@ var class_main_window =
     [ "~MainWindow", "class_main_window.html#ae98d00a93bc118200eeef9f9bba1dba7", null ],
     [ "handleButton2", "class_main_window.html#accf7f1ca023168f0c31d2f9580e8acab", null ],
     [ "handleTreeClicked", "class_main_window.html#a143bb60757a3e3f88e208eedb2af7539", null ],
-    [ "on_actionItem_Options_triggered", "class_main_window.html#ad3d17973d3111f1ba9805217b6ab58cf", null ],
-    [ "on_actionOpen_File_triggered", "class_main_window.html#a3a4866823ec0a9148c03ade962e65cf3", null ],
     [ "startVR", "class_main_window.html#a0c20dc8be5b7925c0e544a72fc17dfa7", null ],
     [ "stopVR", "class_main_window.html#a3fbcc9909cf51a1f8671f495dd493a56", null ]
 ];
